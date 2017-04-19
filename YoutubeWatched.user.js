4825f8a5874d0c14c0c8b5f8de29eca48c3aa3bd
Flavius <highstrike@gmail.com>
2017-04-19 15:08:15 +0300

    Now works for all flows

--- YoutubeWatched.user.js
+++ YoutubeWatched.user.js
@@ -3,23 +3,22 @@
 // @description  Mark unwatched youtube videos that have a watch progress bar as watched
 // @icon         https://raw.githubusercontent.com/highstrike/youtube-watched/master/icon.png
 // @namespace    https://github.com/highstrike/youtube-watched
-// @version      2.1
+// @version      3.0
 // @author       Highstrike
 // @license      GPL; http://www.gnu.org/licenses/gpl.html
 // @require      https://code.jquery.com/jquery-2.2.4.min.js
 // @match        https://www.youtube.com/feed/subscriptions
+// @match        https://www.youtube.com/feed/subscriptions?flow=2
+// @match        https://www.youtube.com/feed/subscriptions?flow=1
 // @grant        none
 // ==/UserScript==
 
 (function() { 'use strict';
-    $('div#browse-items-primary > ol > li').each(function(){
-        var self = $(this),
-            x = self.find('span.resume-playback-background'),
-            y = x.closest('div.yt-lockup-thumbnail'),
-            z = y.find('a:first');
-
-        y.addClass('watched');
-        if(!z.find('div.watched-badge').length > 0)
-            z.append('<div class="watched-badge">WATCHED</div>');
+    $('div.yt-lockup-thumbnail.contains-percent-duration-watched').each(function(){
+        var self = $(this);
+        if(!self.hasClass('watched')) {
+            self.addClass('watched');
+            self.find('a:first').append('<div class="watched-badge">WATCHED</div>');
+        }
     });
 })();
