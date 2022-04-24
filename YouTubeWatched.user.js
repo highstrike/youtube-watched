944ff2b6a608c2813abd357f0c70295003be49e6
Flavius Cosmin <highstrike@gmail.com>
2022-04-25 01:59:12 +0300

    Update and rename YoutubeWatched.user.js to YouTubeWatched.user.js

---
+++ YouTubeWatched.user.js
@@ -0,0 +1,42 @@
+/* globals jQuery */
+
+// ==UserScript==
+// @name         YouTube Watched
+// @description  Mark unwatched youtube videos that have a watch progress bar as watched
+// @icon         https://raw.githubusercontent.com/highstrike/youtube-watched/master/icon.png
+// @namespace    https://github.com/highstrike/youtube-watched
+// @version      5.0
+// @author       Highstrike
+// @license      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
+// @require      https://code.jquery.com/jquery-2.2.4.min.js
+// @match        *://www.youtube.com/*
+// @grant        none
+// ==/UserScript==
+
+(function($) { 'use strict';
+    window.setInterval(function() {
+        $('div#overlays.style-scope.ytd-thumbnail').each(function() {
+            let self = $(this);
+
+            // detect progress
+            if(self.find('ytd-thumbnail-overlay-resume-playback-renderer').length > 0 && self.find('ytd-thumbnail-overlay-playback-status-renderer').length < 1) {
+                self.prepend($('<ytd-thumbnail-overlay-playback-status-renderer>').addClass('style-scope ytd-thumbnail'));
+            }
+
+            // add watched overlay
+            if(self.find('ytd-thumbnail-overlay-playback-status-renderer').length > 0 && self.find('div#watched-white-walker').length < 1) {
+                self.prepend($('<div id="watched-white-walker">').css({
+                    "position": "absolute",
+                    "bottom": 0,
+                    "left": 0,
+                    "top": 0,
+                    "right": 0,
+                    "display": "flex",
+                    "background": "rgba(197, 225, 229, .5)"
+                }));
+
+                self.find('ytd-thumbnail-overlay-time-status-renderer > span#text').text('WATCHED');
+            }
+        });
+    }, 1000);
+})(jQuery);
