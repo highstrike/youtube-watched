847323f018b218b141e60f4b5f70ed4a8b2cd423
Flavius Cosmin <highstrike@gmail.com>
2024-08-06 15:33:46 +0300

    Update YouTubeWatched.user.js

--- YouTubeWatched.user.js
+++ YouTubeWatched.user.js
@@ -6,41 +6,43 @@
 // @icon         https://raw.githubusercontent.com/highstrike/youtube-watched/master/icon.png
 // @author       highstrike
 // @match        *://www.youtube.com/*
-// @require      https://code.jquery.com/jquery-2.2.4.min.js
 // @grant        none
-// @version      2.0.1
+// @version      3.0.0
 // @copyright    2022, highstrike (https://openuserjs.org/users/highstrike)
 // @license      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @updateURL    https://openuserjs.org/meta/highstrike/YouTube_Watched.meta.js
 // @downloadURL  https://openuserjs.org/install/highstrike/YouTube_Watched.user.js
 // ==/UserScript==
 
-/* globals $, jQuery */
+setInterval(() => {
+    document.querySelectorAll('div#overlays.style-scope.ytd-thumbnail').forEach((e) => {
+        // detect progress
+        if (e.querySelector('ytd-thumbnail-overlay-resume-playback-renderer') && !e.querySelector('ytd-thumbnail-overlay-playback-status-renderer')) {
+            const playback = document.createElement('ytd-thumbnail-overlay-playback-status-renderer');
+            playback.classList.add('style-scope', 'ytd-thumbnail');
+            e.prepend(playback);
+        }
 
-(function($) { 'use strict';
-    window.setInterval(function() {
-        $('div#overlays.style-scope.ytd-thumbnail').each(function() {
-            let self = $(this);
+        // add watched overlay
+        if (e.querySelector('ytd-thumbnail-overlay-playback-status-renderer') && !e.querySelector('div#watched-white-walker')) {
+            const overlay = document.createElement('div');
+            overlay.id = 'watched-white-walker';
+            Object.assign(overlay.style, {
+                position: 'absolute',
+                bottom: 0,
+                left: 0,
+                top: 0,
+                right: 0,
+                display: 'flex',
+                background: 'rgba(197, 225, 229, .5)'
+            });
+            e.prepend(overlay);
 
-            // detect progress
-            if(self.find('ytd-thumbnail-overlay-resume-playback-renderer').length > 0 && self.find('ytd-thumbnail-overlay-playback-status-renderer').length < 1) {
-                self.prepend($('<ytd-thumbnail-overlay-playback-status-renderer>').addClass('style-scope ytd-thumbnail'));
+            // overwrite time
+            const time = e.querySelector('ytd-thumbnail-overlay-time-status-renderer > div > badge-shape > div');
+            if (time) {
+                time.textContent = 'WATCHED';
             }
-
-            // add watched overlay
-            if(self.find('ytd-thumbnail-overlay-playback-status-renderer').length > 0 && self.find('div#watched-white-walker').length < 1) {
-                self.prepend($('<div id="watched-white-walker">').css({
-                    "position": "absolute",
-                    "bottom": 0,
-                    "left": 0,
-                    "top": 0,
-                    "right": 0,
-                    "display": "flex",
-                    "background": "rgba(197, 225, 229, .5)"
-                }));
-
-                self.find('ytd-thumbnail-overlay-time-status-renderer > span#text').text('WATCHED');
-            }
-        });
-    }, 1000);
-})(jQuery);
+        }
+    });
+}, 1000);
